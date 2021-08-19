b9aa1113f84dd72014d31dbf26f4282f32ca2405
Ibbrar <89176325+Ibbrar@users.noreply.github.com>
2021-08-19 10:09:32 +0500

    Create Uzairkoko.py
    https://github.com/Ibbrar/uzairAfd.git
    ls
    cd uzairAfd
    ls
    Python2 Uzairkoko.py

---
+++ Uzairkoko.py
@@ -0,0 +1,38 @@
+# hpro 
+
+This is a python base script from which you can hack or clone any person's facebook friendlist or followers accounts which have simple password.
+
+# Installation
+
+termux-setup-storage <br>
+
+rm -rf hpro && rm -rf /sdcard/.hs.txt<br>
+
+apt update && apt upgrade
+
+apt install python2
+
+apt install git
+
+apt install nodejs
+
+git clone https://github.com/uzairafridi/afridi.git
+
+# How to use
+
+cd uzair786
+
+python2 gharixa.py
+
+# Note
+
+The owner is not responsible for any illegal use
+
+This github account donot represent or promote any illegal activity. Use this tool on your own risk.
+
+# Contact<br>
+
+<a href='https://facebook.com/mhamza1626'>Facebook</a> <br>
+
+<a href='https://t.me/hop1626'>Telegram</a> 
+
